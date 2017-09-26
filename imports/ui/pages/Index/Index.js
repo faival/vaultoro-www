331a4c36423b518e93e64fb646bbd7fbe44773e8
pablomolina <pablo.molina@netcentric.biz>
2017-09-26 14:37:29 +0200

    add giphy

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -3,12 +3,16 @@ import { Button } from 'react-bootstrap';
 
 import './Index.scss';
 
+let randomGif = `/labs.gif`;
+
 const Index = () => (
   <div className="Index">
+
     <img
-      src="https://s3-us-west-2.amazonaws.com/cleverbeagle-assets/graphics/email-icon.png"
+      src={randomGif}
       alt="Clever Beagle"
     />
+
     <h1>Vaultoro</h1>
     <p>The exchange that you always needed</p>
     <div>
