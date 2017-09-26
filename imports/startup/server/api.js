eaf935bba88abfa4a705faedc791ae08eb8a3c7f
pablomolina <pablo.molina@netcentric.biz>
2017-09-26 17:55:19 +0200

    request random gifs to giphy

--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -7,3 +7,12 @@ import '../../api/Users/server/methods';
 import '../../api/Users/server/publications';
 
 import '../../api/Utility/server/methods';
+
+
+import { WebApp } from 'meteor/webapp';
+
+
+WebApp.rawConnectHandlers.use("/", function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  return next();
+});
--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button } from 'react-bootstrap';
 
 import './Index.scss';
 
 let randomGif = `/labs.gif`;
 
-const Index = () => (
-  <div className="Index">
+const getRandomGif = () => {
+  return fetch(`https://api.giphy.com/v1/gifs/random?api_key=3CumK1Aa3lPU1FixBbf0S778VAvmIfWB&tag=burrito&rating=G&fmt=json`)
+    .then((response) => response.json())
+    .then(gifObj => {
+      return gifObj.data.image_url
+    })
+}
 
-    <img
-      src={randomGif}
-      alt="Clever Beagle"
-    />
+class Index extends Component {
 
-    <h1>Vaultoro</h1>
-    <p>The exchange that you always needed</p>
-    <div>
-      <Button href="http://cleverbeagle.com/pup">Read the Docs</Button>
-      <Button href="https://github.com/cleverbeagle/pup"><i className="fa fa-star" /> Star on GitHub</Button>
-    </div>
-    <footer>
-      <p>Need help and want to stay accountable building your product? <a href="http://cleverbeagle.com?utm_source=pupappindex&utm_campaign=oss">Check out Clever Beagle</a>.</p>
-    </footer>
-  </div>
-);
+
+  componentDidMount() {
+    getRandomGif().then(gifUrl => {
+      this.gifSrc.setAttribute('src', gifUrl)
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="Index">
+
+        <img
+          ref={ (gifSrc) => {this.gifSrc = gifSrc}}
+          src={randomGif}
+          alt="Clever Beagle"
+        />
+
+        <h1>Vaultoro</h1>
+        <p>The exchange that you always needed</p>
+        <div>
+          <Button href="http://cleverbeagle.com/pup">Read the Docs</Button>
+          <Button href="https://github.com/cleverbeagle/pup"><i className="fa fa-star" /> Star on GitHub</Button>
+        </div>
+        <footer>
+          <p>Need help and want to stay accountable building your product? <a href="http://cleverbeagle.com?utm_source=pupappindex&utm_campaign=oss">Check out Clever Beagle</a>.</p>
+        </footer>
+      </div>
+    )
+  }
+}
 
 export default Index;
